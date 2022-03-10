61c443fdf5495b49975f0d82ccc7a2a7bae38984
worldofelisa <worldofelisa@gmail.com>
2022-03-10 12:35:25 +0000

    Refactored the testing in main.go by deleting commented out code.

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 func main() {
 	barcode := scanImage("./IMG_4779.jpg")
 	bookInfo := getBookInfo(barcode)
-	//fmt.Println(string(bookInfo))
 	bookData := parseBookJson(bookInfo)
 	authInfo := bookData.Authors
 	returnedAuthors := []APIAuthor{}
@@ -24,80 +23,5 @@ func main() {
 
 	//migrates the DB tables!
 	migrateDB(conn)
-	//seedGenres(conn)
-	//
-	//addABook(conn, bookData, returnedAuthors, barcode, 9)
-
-	user := User{}
-	//user.Name = "Elisa"
-	//user.Password = "123456"
-	user.Email = "elisa@elisa.com"
-	//user.Create(conn)
-	user.Retrieve(conn)
-	//fmt.Println(user.Name, user.Password, user.Email)
-	//user.Name = "Princess"
-	//user.Update(conn)
-	////newUserInfo := user.Retrieve(conn)
-	////rowsAddedResponse(newUserInfo.RowsAffected)
-	////printErrorHandler(newUserInfo.Error)
-	////fmt.Println(user.Name, user.Password, user.Email)
-	//user.Delete(conn)
-
-	//pages := PageTracker{}
-	book := Book{}
-	book.Title = "Blood and Chocolate"
-	book.Retrieve(conn)
-	//pages.UserID = user.ID
-	//pages.BookID = book.ID
-	//pages.CurrentPage = 75
-	//pages.Create(conn)
-	//pages.Retrieve(conn)
-	//fmt.Println(pages.CurrentPage)
-	//pages.Update(conn)
-	//tags := []string{
-	//	"werewolf",
-	//	"romance",
-	//	"fantasy",
-	//	"enemies to lovers",
-	//}
-	//for _, tag := range tags {
-	//	t := Tag{}
-	//	t.Name = tag
-	//	t.Create(conn)
-	//	bt := BookTag{}
-	//	bt.BookID = book.ID
-	//	bt.TagID = t.ID
-	//	bt.Create(conn)
-	//}
-	newTags := []string{
-		"paranormal",
-		"YA",
-		"werewolf",
-		"werewolves",
-	}
-	for _, newTag := range newTags {
-		t := Tag{}
-		t.Name = newTag
-		result := t.Retrieve(conn)
-		fmt.Println(result.RowsAffected, newTag)
-		if result.RowsAffected != 0 {
-			continue
-		} else {
-			t.Create(conn)
-			bt := BookTag{}
-			bt.BookID = book.ID
-			bt.TagID = t.ID
-			bt.Create(conn)
-		}
-	}
-
-	//shelf := Shelf{}
-	//shelf.Name = "To Read"
-	//shelf.UserID = user.ID
-	////shelf.Create(conn)
-	//shelf.Retrieve(conn)
-	//shelf.Delete(conn)
-	t := []Tag{}
-	conn.Model(&book).Association("Tag").Find(&t)
-	fmt.Print(t)
+	seedGenres(conn)
 }
