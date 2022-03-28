cef935aee3c06e74926236792245f2207a8b6778
worldofelisa <worldofelisa@gmail.com>
2022-03-28 14:07:41 +0100

    Fixed the issue where only one author was being added instead of all. Fixed issue where enter would return the json API information. Fixed the modal issue so modal now closes after book is added. Fixed my code so the semi-colons are accurate in js.

--- public/js/addbook.js
+++ public/js/addbook.js
@@ -2,16 +2,18 @@ let submitButton = document.getElementById('submit');
 submitButton.addEventListener("click", submittingForm);
 let completeButton = document.getElementById('complete');
 completeButton.addEventListener("click", closingForm);
+var myModal = new bootstrap.Modal(document.querySelector("#myModal"), {});
+var isbn = document.getElementById('InputISBN');
+
+
 
 function submittingForm(e) {
-    var isbn = document.getElementById('InputISBN');
-
+    e.preventDefault();
     axios.post('/addbook', {
         isbn: isbn.value
     })
         .then((response) => {
-            var myModal = new bootstrap.Modal(document.querySelector("#myModal"), {});
-            console.log(response.data)
+            console.log(response.data);
             document.getElementById("bookKey").value = response.data["BookResponse"]["Key"];
             document.getElementById("title").value = response.data["BookResponse"]["Title"];
            let authors = response.data["BookResponse"]["Authors"];
@@ -19,14 +21,14 @@ function submittingForm(e) {
            let authorKey = "";
             for (let x = 0; x < authors.length; x++) {
                if (authorName !== "") {
-                   authorName.concat(",", authors[x]["Name"])
+                  authorName = authorName.concat(", ", authors[x]["Name"]);
                } else {
-                   authorName = authors[x]["Name"]
+                   authorName = authors[x]["Name"];
                }
                if (authorKey !== "") {
-                   authorKey.concat(",", authors[x]["Key"])
+                   authorKey.concat(",", authors[x]["Key"]);
                } else {
-                   authorKey = authors[x]["Key"]
+                   authorKey = authors[x]["Key"];
                }
            }
             document.getElementById("author").value = authorName;
@@ -34,28 +36,26 @@ function submittingForm(e) {
             document.getElementById("pages").value = response.data["BookResponse"]["PageNumber"];
             let genreDOM = document.getElementById("genre");
             for(let i = 0; i < response.data["Genres"].length; i++){
-                let option = document.createElement("option")
+                let option = document.createElement("option");
                 option.value = i;
                 option.text = response.data["Genres"][i];
                 genreDOM.appendChild(option);
             }
             myModal.show();
-
-            console.log(response)
         })
         .catch((error) => {
-            console.log(error)
+            console.log(error);
         });
 }
 
 function closingForm(e) {
+    e.preventDefault();
     var review = document.getElementById("review");
     var tags = document.getElementById("tags");
     var title = document.getElementById("title");
     var author = document.getElementById("authorsKeys");
     var pages = document.getElementById("pages");
     var genre = document.getElementById("genre");
-    console.log(review, tags, title, author, pages, genre);
     //separate the tags out at the commas into an array.
     axios.post('/confirmbook', {
         review: review.value,
@@ -64,10 +64,13 @@ function closingForm(e) {
         authors: author.value,
         pages: pages.value,
         genre: genre.value,
-        isbn: document.getElementById("isbn").value,
+        isbn: isbn.value,
         key: document.getElementById("bookKey").value
     })
+        .then((response) => {
+            myModal.hide();
+            isbn.value = "";
+        })
 }
-    //TODO close the modela once the information is sent back with a popup message that says book added.
-    //book should then pop up on screen with the option to add book with information, edit information, or manually add book
+
 
